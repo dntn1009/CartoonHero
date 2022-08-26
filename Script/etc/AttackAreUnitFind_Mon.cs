c5d0c8ffe6bd578b1bd623251e34316b2e697db7
dntn1009 <67273308+dntn1009@users.noreply.github.com>
2022-08-26 15:21:27 +0900

    Cartoon_Hero Script
    Use scripts

---
+++ Script/etc/AttackAreUnitFind_Mon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAreUnitFind_Mon : MonoBehaviour
+{
+    // 플레이어 공격시 유닛 찾기
+    List<GameObject> m_unitList = new List<GameObject>();
+
+    public List<GameObject> UnitList { get { return m_unitList; } }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_unitList.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_unitList.Remove(other.gameObject);
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
